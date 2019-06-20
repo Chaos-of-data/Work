167784b212239a7a66ac35f5354334269d4f11af
Chaos-of-data <52020072+Chaos-of-data@users.noreply.github.com>
2019-06-20 13:43:54 +0900

    Add files via upload

---
+++ First.R
@@ -0,0 +1,27 @@
+print("Hello, K") #print함수
+print(5+10) #덧셈
+
+print("5+10")
+print('5+10') #문자는사칙연산안돼
+print(5.5+10.9)
+
+print(5<10)
+print(5>10)
+
+print(5<10&&100>50)
+print(5>10||100>50)
+
+
+a <- 5
+b <- 10
+print(a+b)
+
+print(a)
+print(b)
+
+c<-"Oliver" #연두색은문자
+d<-TRUE #다홍색은숫자
+
+print(c)
+print(d)
+
